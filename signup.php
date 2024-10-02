5fc0ea8cae31e0a8e21fb1be35b2e6ff914067c0
Cloud User <NickPhaita@Centos-8.3>
2024-10-02 21:30:55 +0000

    Modified title for signup.php

--- signup.php
+++ signup.php
@@ -15,7 +15,7 @@ if(isset($_SESSION['user_email'])){
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up - Footmarkz.com</title>
+    <title>Login/Signup</title>
     <link rel="stylesheet" href="style.css" media="all" type="text/css">
 </head>
 
