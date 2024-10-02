e6b12f0d4b692ed58b6b5d911be515b87e3b000e
Cloud User <NickPhaita@Centos-8.3>
2024-10-02 20:46:34 +0000

    Modified db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,5 +1,5 @@
 <?php
-$db_connection = mysqli_connect("localhost","root","","login_registration");
+$db_connection = mysqli_connect("localhost","root","root","CAT1");
 // Check connection
 if (mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
